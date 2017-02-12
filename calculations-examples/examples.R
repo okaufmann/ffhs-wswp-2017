b6f1baddf527514f3cf330907fba87473d651ac4
Oliver Kaufmann <okaufmann91@gmail.com>
2017-02-12 21:20:57 +0100

    Initial commit

---
+++ calculations-examples/examples.R
@@ -0,0 +1,23 @@
+print("Define vector x")
+c(10.4, 5.6, 3.1, 6.4, 21.7) -> x
+print(x)
+
+print("define another vector y")
+c(5, 3, 2, 3, 14) -> y
+print(y)
+
+print("Divide 1 by vector x")
+z <- 1/x
+print(z)
+
+print("substrate y from vector")
+z <- x-y
+print(z)
+
+print("change sign of vector x values")
+z <- x*-1
+print(z)
+
+print("print euler's number")
+e = exp(1)
+print(e)
